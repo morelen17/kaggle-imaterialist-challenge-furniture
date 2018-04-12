8891a39ec1f37285203a44d8fcdc42a8b303f8c5
lenar <lenar-kazan@yandex.ru>
2018-04-13 00:43:39 +0300

    "Train" part changed

--- wide_resnet.py
+++ wide_resnet.py
@@ -150,40 +150,83 @@ def train_dataset():
     ds = tf.data.TextLineDataset(['./data/train_actual_extended.csv']).skip(1)
     ds = ds.shuffle(buffer_size=TRAIN_SET_ACTUAL_SIZE)  # or 10k is OKNOTOK ???
     ds = ds.map(lambda x: _map_func(x, train_data_folder), num_parallel_calls=cpu_count())
-    ds = ds.repeat(NUM_EPOCH)
+    # ds = ds.repeat(NUM_EPOCH)
     ds = ds.batch(BATCH_SIZE)
     return ds
+
+
+def validation_dataset():
+    train_data_folder = './data/images/validation/'
+    ds = tf.data.TextLineDataset(['./data/validation_actual_extended.csv']).skip(1)
+    ds = ds.shuffle(buffer_size=TRAIN_SET_ACTUAL_SIZE)  # or 10k is OKNOTOK ???
+    ds = ds.map(lambda x: _map_func(x, train_data_folder), num_parallel_calls=cpu_count())
+    ds = ds.batch(BATCH_SIZE)
+    return ds
+
+
+def add_stats(loss, accuracy):
+    tf.summary.scalar('loss', loss)
+    tf.summary.scalar('accuracy', accuracy)
+    return tf.summary.merge_all()
 
 
 def train():
     global_step = tf.Variable(0, name='global_step', trainable=False)
 
     train_ds = train_dataset()
+    validation_ds = validation_dataset()
     iterator = tf.data.Iterator.from_structure(train_ds.output_types, train_ds.output_shapes)
-    train_initializer_op = iterator.make_initializer(train_ds)
-
     img_batch, label_batch = iterator.get_next()
 
     model = WRN()
     logits = model.build(img_batch)
     loss_op, train_op = model.add_loss(label_batch, global_step)
     accuracy_op = tf.metrics.accuracy(tf.argmax(logits, 1), tf.argmax(label_batch, 1))
+    statistics = add_stats(loss_op, accuracy_op[1])
+
+    training_init_op = iterator.make_initializer(train_ds)
+    validation_init_op = iterator.make_initializer(validation_ds)
+
+    saver = tf.train.Saver()
 
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
         sess.run(tf.local_variables_initializer())  # to use tf.metrics.accuracy
-        with tqdm() as counter:
-            sess.run(train_initializer_op)
-            try:
-                while True:
-                    step, loss, accuracy, _ = sess.run([global_step, loss_op, accuracy_op, train_op])
-                    counter.set_postfix({
-                        "loss": "{:.6}".format(loss),
-                        "accuracy": "{:.6}".format(accuracy[1])
-                    })
-                    counter.update(1)
-            except tf.errors.OutOfRangeError:
-                print("Finished training")
+
+        statistics_writer = tf.summary.FileWriter('logs-wrn', sess.graph)
+
+        for epoch in range(1, NUM_EPOCH + 1):
+            # train
+            with tqdm() as counter:
+                sess.run(training_init_op)
+                step = 0
+                try:
+                    while True:
+                        step, loss, accuracy, _ = sess.run([global_step, loss_op, accuracy_op, train_op])
+                        counter.set_postfix({
+                            "loss": "{:.6}".format(loss),
+                            "accuracy": "{:.6}".format(accuracy[1])
+                        })
+                        counter.update(1)
+                        if step % 500 == 0:
+                            statistics_writer.add_summary(sess.run(statistics), step)
+
+                except tf.errors.OutOfRangeError:
+                    print("End of epoch # {}".format(epoch))
+                    saver.save(sess, 'logs-wrn/model.ckpt', global_step=step)
+
+            # validate
+            with tqdm() as counter:
+                sess.run(validation_init_op)
+                try:
+                    while True:
+                        accuracy, _ = sess.run([accuracy_op, loss_op])
+                        counter.set_postfix({
+                            "accuracy": "{:.6}".format(accuracy[1])
+                        })
+                        counter.update(1)
+                except tf.errors.OutOfRangeError:
+                    print("End of validation after epoch # {}".format(epoch))
 
 
 if __name__ == "__main__":
